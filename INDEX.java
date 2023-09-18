e9b27653ac00cb498878f5ec253ca3e041ce51ee
ShimiyanaJakroda <shimiyanajakroda@gmail.com>
2023-09-18 10:32:57 +0200

    Update INDEX.java
    update 2

--- INDEX.java
+++ INDEX.java
@@ -13,20 +13,20 @@ public class INDEX {
 
         if (x > 0 && y > 0) {
             if (BMI < 18.5) {
-                JOptionPane.showMessageDialog(null, "You are within the Underweight range");
+                JOptionPane.showMessageDialog(null, "You are within the Underweight range less than 18.5");
 
             } // the first Range of the IBM
 
             if (BMI > 18.5 && BMI <= 24.9) {
-                JOptionPane.showMessageDialog(null, "You are within the Healthy weight range:");
+                JOptionPane.showMessageDialog(null, "You are within the Healthy weight range withis within 18.5 t0 24.9:");
             } // The second Range of the IBM
 
             if (BMI > 25.0 && BMI <= 29.9) {
-                JOptionPane.showMessageDialog(null, "You are within the over weight range:");
+                JOptionPane.showMessageDialog(null, "You are within the over weight range within 25.0 to 29.9:");
             } // The Third Range of the IBM
 
             if (BMI > 30) {
-                JOptionPane.showMessageDialog(null, "You are Obsese kindly Seek Medical Attention:");
+                JOptionPane.showMessageDialog(null, "You are Obsese kindly Seek Medical Attention(Above 30):");
             } // The fourth Range of the IBM
 
         }
